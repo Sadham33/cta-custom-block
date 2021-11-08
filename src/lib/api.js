8156a35a86f231c6d50dab4f73e123332f638605
Sadham Hussain <sadhhussain@paypal.com>
2021-11-08 19:02:42 +0530

    button changes

--- src/lib/api.js
+++
@@ -1,5 +0,0 @@
-export async function getModule() {
-    const response = await fetch(`https://emailmodules.azurewebsites.net/harmonyTemplates/components/primaries/p1.1.js`);
-    debugger;
-    return await response.json();
-  }
